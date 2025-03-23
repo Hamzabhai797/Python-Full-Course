d1869e8233083700d0c442604c9805a0baa3e4fc
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-03-23 16:19:24 +0500

    start chp 03

---
+++ chapter 03/list.py
@@ -0,0 +1,16 @@
+# list = [40, 50,60, 80, 100]
+# print(list)
+
+# list = [40, 50,60, 80, 100]
+# print(list[3])
+
+# list = [40, 50,60, 80, 100]
+# print(list[3], list[2])
+
+# student = ['Hamza', 21, 'Karachi']
+# print(student)
+
+# student = ['Hamza', 21, 'Karachi']
+# student[0] = 'Khan'
+# print(student)          # ['Khan', 21, 'Karachi']
+
