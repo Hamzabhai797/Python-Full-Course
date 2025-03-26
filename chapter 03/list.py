86677bac78aa18ace8a51128fce452d9debe4c28
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-03-26 10:50:54 +0500

    more oractice in list

--- chapter 03/list.py
+++ chapter 03/list.py
@@ -16,6 +16,35 @@
 
 #list methods
 
+# list = [40, 50,60, 80, 100]
+# list.append(120)
+# print(list)
 
+# list = [40, 50, 10, 80, 100]
+# list.sort()
+# print(list)
 
-# time = "3:35:00"
+# list = [40, 50, 10, 80, 100]
+# list.sort(reverse=True)
+# print(list)  # [100, 80, 50, 40, 10] descending order
+
+# list = [40, 50, 10, 80, 100]
+# list.reverse()
+# print(list)
+
+# list = [40, 50, 10, 80, 100]
+# list.pop()
+# print(list)
+
+# list = [40, 50, 10, 80, 100]
+# list.remove(10)
+# print(list)
+
+# list = [40, 50, 10, 80, 100]
+# list.clear()
+# print(list)
+
+# list = [40, 50, 10, 80, 100]
+# list.copy()
+# print(list)
+# time = "3:45:00"
