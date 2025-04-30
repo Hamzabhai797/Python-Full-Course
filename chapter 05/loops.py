ccafd797c44c4cb0d0ab7db7a5eeac3702ffe566
hamzabhai797 <hamzanoor11111122@gmail.com>
2025-04-30 10:58:21 +0500

    while loop complete

---
+++ chapter 05/loops.py
@@ -0,0 +1,33 @@
+# while loop
+
+# count = 1
+# while count <= 5:
+#     print("Hello Hamza")
+#     count += 1
+
+# i = 1
+# while i <= 100:
+#     print("Hi! hamza")
+#     i += 1
+
+# i = 1
+# while i <= 50:
+#     print(i)
+#     i += 1
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# time = 5:16:25
---
+++ chapter 05/practice.py
@@ -0,0 +1,26 @@
+# i = 1
+# while i <= 100:
+#     print(i)
+#     i += 1
+
+# i = 100
+# while i >= 1:
+#     print(i)
+#     i -= 1
+
+# table = int(input("Enter a anumber: "))
+# i = 1
+# while i <= 10:
+#     print(f"{table} x {i} = {table * i}")
+#     i += 1
+
+# i = 1
+# while i <= 10:
+#     print(4*i)
+#     i += 1
+
+num = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+idx = 0
+while idx < len(num):
+    print(idx)
+    idx += 1
